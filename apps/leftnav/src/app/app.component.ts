d7ef379d1f992e701d41aacc30ae6319713b258e
Anuroop Joy <anuroop.joy@hrblock.com>
2021-05-22 17:37:16 +0530

    rename component

--- apps/leftnav/src/app/app.component.ts
+++ apps/leftnav/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng12-module-fed-root',
+  selector: 'left-nav',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
